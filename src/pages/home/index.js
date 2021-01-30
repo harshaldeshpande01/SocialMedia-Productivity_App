1c0196f6baf8bedcf21a74dcd26c1507be9aa610
Harshal Deshpande <harshal25012001@gmail.com>
2021-01-30 10:29:53 +0530

    trial

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, {useContext} from "react";
 import "./style.css";
-import { SignInBtn } from "../../components";
 import { Navbar, CreatePost } from "../../containers";
 import Feed from "../../containers/feed";
+import { UserContext } from "../../contexts/user";
 
 export default function Home() {
+  const [user, setUser] = useContext(UserContext).user;
+
   return (
     <div className="home">
-      <Navbar />
-      <CreatePost />
-      <Feed />
+      {user?
+        <>
+          <Navbar />
+          <CreatePost />
+          <Feed />
+        </>
+        :
+        <Navbar />
+      }
     </div>
   );
 }
